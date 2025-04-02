41607e1b1b9cbe9b5c6d9e7dd1f0c2d4c9837571
Will Beason <willbeason@gmail.com>
2025-04-02 12:11:59 -0500

    Fix validation logic
    Checks were performed in an incorrect order.
    
    Signed-off-by: Will Beason <willbeason@gmail.com>

--- fix-phone-num.py
+++ fix-phone-num.py
@@ -1,16 +1,16 @@
 import pytest
 
 def fix_phone_num(phone_num_to_fix):
-    if len(phone_num_to_fix) != 10:
-        raise ValueError(f"phone number must be length 10; got \"{phone_num_to_fix}\" which is of length {len(phone_num_to_fix)}")
-    if not phone_num_to_fix.isdigit():
-        raise ValueError(f"phone number must only contain digits; got \"{phone_num_to_fix}\"")
     if len(phone_num_to_fix) == 11:
         if phone_num_to_fix[0] != "1":
             raise ValueError(f"phone numbers of length 11 must begin with a 1, got \"{phone_num_to_fix}\"")
         # Strip off the country code "1" from the beginning.
         # Resulting string should be of length 10 to work with formatting code below.
         phone_num_to_fix = phone_num_to_fix[1:]
+    if len(phone_num_to_fix) != 10:
+        raise ValueError(f"phone number must be length 10; got \"{phone_num_to_fix}\" which is of length {len(phone_num_to_fix)}")
+    if not phone_num_to_fix.isdigit():
+        raise ValueError(f"phone number must only contain digits; got \"{phone_num_to_fix}\"")
 
     # given "5125558823". Split the parts, then recombine and return
     area_code = phone_num_to_fix[0:3]  # 512 (first three digits)
